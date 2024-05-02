2aac5f05f33dd6ef550df449a0bc2280bc86ecce
Renceskieee <quiniano.lp.bsinfotech@gmail.com>
2024-05-02 23:17:13 +0800

    second commit

---
+++ IPAT2CQUINIANO/earnist/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EarnistConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'earnist'
---
+++ IPAT2CQUINIANO/earnist/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def main(request):
+    #what you want to do
+
+    return  render(request, 'main.html')
