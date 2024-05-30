a859197eec64f7b62d398d57b91c7c6d2aec6c17
Renceskieee <quiniano.lp.bsinfotech@gmail.com>
2024-05-30 23:37:19 +0800

    Posgre commit

--- IPAT2CQUINIANO/IPAT2CQUINIANO/settings.py
+++ IPAT2CQUINIANO/IPAT2CQUINIANO/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'IPAT2CQUINIANO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'EARNIST',
+        'USER': 'postgres',
+        'PASSWORD': 'Laurence027',
+        'PORT': '5432',
     }
 }
 
